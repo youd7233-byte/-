12faa9cdd87970572f3749244a6cd7da3db03c43
Youcef <youcef@hirafi.dz>
2026-06-10 16:38:17 +0100

    fix: remove sqlite adapter which was causing npm install failure on Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,8 @@
 import "server-only";
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const prismaClientSingleton = () => {
-  const url = process.env.DATABASE_URL || "file:./dev.db";
-  const adapter = new PrismaBetterSqlite3({ url });
-
   return new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 };
